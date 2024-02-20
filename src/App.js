57867e4f6f77f6ce76120e322dd474b2ce09d1b1
atishoff012803 <95717588+Atish280301@users.noreply.github.com>
2024-02-20 12:49:43 +0530

    Commit - 04
    1. Implement Loading Logic
    2. Initially it is showing "Found No Movies!"
    3. In Time Of Loading It Will Show "Loading..."
    4. then it will show the movie list.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, SetMovies] = useState([])
+  const [movies, SetMovies] = useState([]);
+  const [IsLoading, SetIsLoading] = useState(false);
   const FetchMovies = async () => {
+    SetIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json();
       const transformedMovies = data.results.map(moviedata => {
@@ -17,6 +19,7 @@ function App() {
         };
       });
       SetMovies(transformedMovies);
+      SetIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -24,7 +27,9 @@ function App() {
         <button onClick={FetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!IsLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!IsLoading && movies.length === 0 && <p>Found No Movies!</p>}
+        {IsLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
